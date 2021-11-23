9d3355f8d38441469898a2e41648335c6f4cbff7
“homer” <just_2@163.com>
2021-11-23 14:47:34 +0800

    反射-代理类的运用

--- src/com/homer/reflection/proxy/ProxyTraceTest.java
+++ src/com/homer/reflection/proxy/ProxyTraceTest.java
@@ -12,9 +12,15 @@ public class ProxyTraceTest {
         for (int i = 0; i < numbers.length; i++) {
 
             Integer number = i + 1;
+            /**
+             * 其本质是用Proxy.newProxyInstance()方法创建了代理对象，然后调用者其实是用的代理对象来实现业务，这对调用者是透明的。
+             * Proxy.newProxyInstance()需要传入三个参数。
+             * 1、ClassLoader，保持默认的类加载器，传入null即可。
+             * 2、Class[]要监测或处理的接口事件对应的一组接口。
+             * 3、InvocationHandler。在InvocationHandler中就可以添加要实现的业务逻辑（本实例中是实现跟踪，就打印了一些调用信息）。
+             */
             InvocationHandler traceHandler = new TraceHandler(number);
             Object proxy = Proxy.newProxyInstance(null,new Class[] {Comparable.class}, traceHandler);
-
             numbers[i] = proxy;
         }
 
