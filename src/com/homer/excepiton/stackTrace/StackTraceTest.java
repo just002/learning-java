7a70da1253ae609910965c8ceb6ec031a236133d
Homer <just_2@163.com>
2021-11-25 23:45:28 +0800

    断言

--- src/com/homer/excepiton/stackTrace/StackTraceTest.java
+++ src/com/homer/excepiton/stackTrace/StackTraceTest.java
@@ -1,6 +1,8 @@
 package com.homer.excepiton.stackTrace;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StackTraceTest {
 
@@ -41,6 +43,18 @@ public class StackTraceTest {
         int input = scanner.nextInt();
 
         System.out.println("1-" + input + "的阶乘结果为：" + factorial(input));
+
+
+        /**
+         * 测试断言功能
+         * 断言默认应该是关闭，需要在IDE的VM options中加入-ea参数开启，否则相关代码会被忽略。
+         */
+        String test = "100";
+        Pattern p = Pattern.compile("[0-9]*");
+        Matcher m = p.matcher(test);
+        assert m.matches();
+
+        Integer.valueOf(test);
     }
 
 }
