c66e56455e49fd0f465d92e96d663b8652bc1f88
Homer <just_2@163.com>
2022-02-10 22:33:44 +0800

    Treeset测试类

---
+++ src/com/homer/connection/treeSet/Item.java
@@ -0,0 +1,54 @@
+package com.homer.connection.treeSet;
+
+import java.util.Objects;
+
+public class Item implements Comparable<Item>{
+
+    private int partNumber;
+    private String description;
+
+    public Item(int partNumber, String description) {
+        this.partNumber = partNumber;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "partNumber=" + partNumber +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return partNumber == item.partNumber &&
+                Objects.equals(description, item.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(partNumber, description);
+    }
+
+    @Override
+    public int compareTo(Item o) {
+        //return this.partNumber - o.partNumber;
+
+        /**
+         * 作者是直接调用integer的比较方法，更为规范。本身我自己对比较的规则就掌握的不好，只是凭借一点点的印象。
+         */
+        int diff = Integer.compare(this.partNumber, o.partNumber);
+        /**
+         * 如果编号一样，再以description作为排序
+         */
+        return diff != 0 ? diff : description.compareTo(o.description);
+    }
+}
---
+++ src/com/homer/connection/treeSet/TreeSetTest.java
@@ -0,0 +1,31 @@
+package com.homer.connection.treeSet;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class TreeSetTest {
+    public static void main(String[] args) {
+        Item a = new Item(100, "cpu");
+        Item b = new Item(80, "mem");
+        Item c = new Item(50, "disk");
+        Item e = new Item(50, "abc");
+        Item f = new Item(50, "zzz");
+
+        TreeSet<Item> treeSet = new TreeSet<>();
+        treeSet.add(a);
+        treeSet.add(b);
+        treeSet.add(c);
+        treeSet.add(e);
+        treeSet.add(f);
+
+        System.out.println(treeSet);
+
+        //直接使用自定义的比较器，而不是item的比较器
+        TreeSet<Item> treeSet1 = new TreeSet<>(Comparator.comparing(Item::getDescription));
+        treeSet1.addAll(treeSet);
+        System.out.println(treeSet1);
+
+    }
+
+
+}
